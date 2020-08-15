d59c16a3aae7f9f38dc218116265e2b81ede8a36
MariuzM <mariuzz7789@gmail.com>
2020-08-15 15:17:04 +0100

    test asd aasd 111

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App: React.FC = (): JSX.Element => {
-  return <div> sadas asdas asdas asdas sadsa d asda s sd as</div>
+  return <div> asd sssssssss dsa ds asda asda s sd as</div>
 }
 
 export default App

ca63657991e7bc895191d2623d2d2cadd65857ad
MariuzM <mariuzz7789@gmail.com>
2020-08-15 15:18:47 +0100

    sdfsd dsfs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App: React.FC = (): JSX.Element => {
-  return <div> asd sssssssss dsa ds asda asda s sd as</div>
+  return <div> asd sdddddddddddddddddd dsa ds asda asda s sd as</div>
 }
 
 export default App
